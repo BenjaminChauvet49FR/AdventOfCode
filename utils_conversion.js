077ad0d335e12c4ccef2d8e774d65cdbb7d0c597
Benjamin <benjchauvet@gmail.com>
2023-12-13 23:25:28 +0100

    Oh, added this

---
+++ utils_conversion.js
@@ -0,0 +1,56 @@
+// Hexadecimal/binary conversion !!!
+
+function hexadecimalToBinary(p_string) {
+	var answer = [];
+	var val, charac;
+	for (var i = 0 ; i < p_string.length ; i++) {
+		charac = p_string[i];
+		if (charac >= 'A' && charac <= 'F') {
+			val = charToASCIIRelativePosition(charac, 'A')+10;
+		} else {
+			val = charToASCIIRelativePosition(charac,'0');
+		}
+		hexadecimalToBinaryAddDigits(answer, val);
+	}
+	return answer;
+}
+
+function hexadecimalToBinaryAddDigits(p_binaryArray, p_value) {
+	var bit = p_value >= 8;
+	p_binaryArray.push(bit);
+	if (bit) { p_value -= 8 };
+	bit = p_value >= 4;
+	p_binaryArray.push(bit);
+	if (bit) { p_value -= 4 };
+	bit = p_value >= 2;
+	p_binaryArray.push(bit);
+	if (bit) { p_value -= 2 };
+	p_binaryArray.push(p_value == 1);
+}
+
+function numberToBinary(p_number) {
+	var number = p_number;
+	var answer = [];
+	while (number > 0) {
+		answer.push([number % 2 == 1]);
+		number = number >> 1;
+	}
+	return answer;
+}
+
+// p_number must be between 0 and 2**32-1
+function numberToHexadecimal(p_number) {
+	const digits = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+	var answerList = [];
+	var number = p_number;
+	while(number > 0) {		
+		answerList.push(number % 16);
+		number = Math.floor(number/16);
+	}
+	answerList = answerList.reverse();
+	var answer = "";
+	answerList.forEach(a => {
+		answer += digits[a];
+	});
+	return answer;
+}
