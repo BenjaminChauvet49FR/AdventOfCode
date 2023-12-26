d64e0922e27ee6ca468ede5424474bda2e199a49
Benjamin <benjchauvet@gmail.com>
2023-12-26 11:31:39 +0100

    AOC 2019 Intcode : modified computer such that it supports relative writing and extra addresses + reviewed parameters

--- year2019_intcode.js
+++ year2019_intcode.js
@@ -10,6 +10,8 @@ function newIntCodeReader(p_input) {
 	answer.input = (p_input ? p_input.slice() : []);
 	answer.inputIndex = 0;
 	answer.pending = false;
+	answer.relativeBase = 0;
+	answer.extraMemory = [];
 	return answer;
 }
 
@@ -28,23 +30,22 @@ function readIntCodeStep(p_program, p_reader) {
 	}
 	p_reader.emitsOutput = false;
 	ri = p_reader.pointer;
-	if (ri >= p_program.length || ri < 0) {
+	if (ri < 0) {
 		return INTCODE_INCORRECT_VALUE
+	}
+	if (ri > p_program.length) {
+		return INTCODE_INCORRECT_VALUE;
 	}
 	var opcode = p_program[ri];
 	var operator = opcode % 100;
-	var parameterModes;
+	var parameters;
 	if (operator == 1) { // Addition
-		parameterModes = getIntCodeParamModes(opcode, 2);
-		const value1 = parameterModes[0] == 0 ? p_program[p_program[ri+1]] : p_program[ri+1];
-		const value2 = parameterModes[1] == 0 ? p_program[p_program[ri+2]] : p_program[ri+2];
-		p_program[p_program[ri+3]] = value1 + value2;
+		parameters = getIntCodeParameters(opcode, 3, p_program, p_reader);
+		writeIntCode(p_program, p_reader, p_program[ri+3], parameters[0] + parameters[1]); // TODO Warning : this is not the address !
 		p_reader.pointer += 4;
 	} else if (operator == 2) { // Multiplication
-		parameterModes = getIntCodeParamModes(opcode, 2);
-		const value1 = parameterModes[0] == 0 ? p_program[p_program[ri+1]] : p_program[ri+1];
-		const value2 = parameterModes[1] == 0 ? p_program[p_program[ri+2]] : p_program[ri+2];
-		p_program[p_program[ri+3]] = value1 * value2;		
+		parameters = getIntCodeParameters(opcode, 3, p_program, p_reader);
+		writeIntCode(p_program, p_reader, p_program[ri+3], parameters[0] * parameters[1]);
 		p_reader.pointer += 4;
 	} else if (operator == 3) { // Input
 		if (p_reader.inputIndex == p_reader.input.length) {
@@ -52,45 +53,42 @@ function readIntCodeStep(p_program, p_reader) {
 			p_reader.inputIndex = 0;
 			p_reader.input = [];
 		} else {			
-			p_program[p_program[ri+1]] = p_reader.input[p_reader.inputIndex]; // Always in position mode !
+			parameters = getIntCodeParameters(opcode, 1, p_program, p_reader);
+			writeIntCode(p_program, p_reader, p_program[ri+1], p_reader.input[p_reader.inputIndex]);
 			p_reader.inputIndex++;
 			p_reader.pointer += 2;
 		}
 	} else if (operator == 4) { // Output
-		parameterModes = getIntCodeParamModes(opcode, 1);
+		parameters = getIntCodeParameters(opcode, 1, p_program, p_reader);
 		p_reader.emitsOutput = true;
-		p_reader.output = (parameterModes[0] == 0 ? p_program[p_program[ri+1]] : p_program[ri+1]);
+		p_reader.output = parameters[0];
 		p_reader.pointer += 2;
 	} else if (operator == 5) { // Jump-if-true
-		parameterModes = getIntCodeParamModes(opcode, 2);
-		const value1 = parameterModes[0] == 0 ? p_program[p_program[ri+1]] : p_program[ri+1];
-		const value2 = parameterModes[1] == 0 ? p_program[p_program[ri+2]] : p_program[ri+2];
-		if (value1 != 0) {
-			p_reader.pointer = value2;
+		parameters = getIntCodeParameters(opcode, 2, p_program, p_reader);
+		if (parameters[0] != 0) {
+			p_reader.pointer = parameters[1];
 		} else {			
 			p_reader.pointer += 3;
 		}
 	} else if (operator == 6) { // Jump-if-false
-		parameterModes = getIntCodeParamModes(opcode, 2);
-		const value1 = parameterModes[0] == 0 ? p_program[p_program[ri+1]] : p_program[ri+1];
-		const value2 = parameterModes[1] == 0 ? p_program[p_program[ri+2]] : p_program[ri+2];
-		if (value1 == 0) {
-			p_reader.pointer = value2;
+		parameters = getIntCodeParameters(opcode, 2, p_program, p_reader);
+		if (parameters[0] == 0) {
+			p_reader.pointer = parameters[1];
 		} else {			
 			p_reader.pointer += 3;
 		}
 	} else if (operator == 7) { // less than
-		parameterModes = getIntCodeParamModes(opcode, 2);
-		const value1 = parameterModes[0] == 0 ? p_program[p_program[ri+1]] : p_program[ri+1];
-		const value2 = parameterModes[1] == 0 ? p_program[p_program[ri+2]] : p_program[ri+2];
-		p_program[p_program[ri+3]] = (value1 < value2 ? 1 : 0);
+		parameters = getIntCodeParameters(opcode, 3, p_program, p_reader);
+		writeIntCode(p_program, p_reader, p_program[ri+3], (parameters[0] < parameters[1] ? 1 : 0) );
 		p_reader.pointer += 4;
 	} else if (operator == 8) { // equal
-		parameterModes = getIntCodeParamModes(opcode, 2);
-		const value1 = parameterModes[0] == 0 ? p_program[p_program[ri+1]] : p_program[ri+1];
-		const value2 = parameterModes[1] == 0 ? p_program[p_program[ri+2]] : p_program[ri+2];
-		p_program[p_program[ri+3]] = (value1 == value2 ? 1 : 0);
+		parameters = getIntCodeParameters(opcode, 3, p_program, p_reader);
+		writeIntCode(p_program, p_reader, p_program[ri+3], (parameters[0] == parameters[1] ? 1 : 0) );
 		p_reader.pointer += 4;
+	} else if (operator == 9) { // relative base adjust
+		parameters = getIntCodeParameters(opcode, 2, p_program, p_reader);
+		p_reader.relativeBase += parameters[0];
+		p_reader.pointer += 2;
 	} else if (operator == 99) { // End
 		p_reader.end = true;
 	} else {		
@@ -99,21 +97,31 @@ function readIntCodeStep(p_program, p_reader) {
 	return 0;
 }
 
-function getIntCodeParamModes(p_opcode, p_nbParams) {
+function getIntCodeParameters(p_opcode, p_nbParams, p_program, p_reader) {
 	var number = Math.floor(p_opcode/100);
-	var answer = [];
+	var ri = p_reader.pointer;
+	var modes = [];
 	while (number > 0) {
-		answer.push(number % 10);
+		modes.push(number % 10);
 		number = Math.floor(number/10);
 	}
-	while (answer.length < p_nbParams) {
-		answer.push(0);
+	while (modes.length < p_nbParams) {
+		modes.push(0);
+	}
+	var answer = [];
+	for (var i = 0 ; i < p_nbParams ; i++) {
+		switch (modes[i]) { // Read "arguments" from index ri+1 to ri+p_nbParams
+			case 0 : answer.push( readIntCode(p_program, p_reader, (readIntCode(p_program, p_reader, ri+i+1))) ); break;
+			case 1 : answer.push( readIntCode(p_program, p_reader, ri+i+1) ); break;
+			case 2 : answer.push( readIntCode(p_program, p_reader, (readIntCode(p_program, p_reader, ri+i+1)+p_reader.relativeBase)) ); break;
+			// default : should not happen !
+		}
 	}
 	return answer;
 }
 
 // Warning : alters data in entrance
-function readIntCode(p_program, p_reader, p_stopFunction) {
+function readIntCodeProgram(p_program, p_reader, p_stopFunction) {
 	if (!p_reader) {
 		p_reader = newIntCodeReader();
 	}
@@ -129,4 +137,82 @@ function readIntCode(p_program, p_reader, p_stopFunction) {
 		}
 	}
 	return INTCODE_INCORRECT_VALUE;
+}
+
+function copyIntoBigIntList(p_list) {
+	var answer = [];
+	for (var i = 0 ; i < p_list.length ; i++) {
+		answer.push(BigInt(p_list[i]));
+	}
+	return answer;
+}
+
+function writeIntCode(p_program, p_reader, p_address, p_value) {
+	if (p_address < 0) {
+		console.log("Alert : Invalid writing !");
+		return INTCODE_INCORRECT_VALUE;
+	}
+	if (p_address < p_program.length) {
+		p_program[p_address] = p_value;
+	} else {
+		if (p_reader.extraMemory.length == 0) {
+			p_reader.extraMemory.push({address : p_address, value : p_value});
+		} else { 
+			const indexF = findIndexExtraMemoryIntCode(p_reader.extraMemory, p_address);
+			if (indexF.found) {
+				p_reader.extraMemory[indexF.index] = {address : p_address, value : p_value};
+			} else {				
+				p_reader.extraMemory.splice(indexF.index, 0, {address : p_address, value : p_value});
+			}
+		}
+	}
+}
+
+function readIntCode(p_program, p_reader, p_address) {
+	if (p_address < 0) {
+		console.log("Alert : Invalid reading !");
+		return INTCODE_INCORRECT_VALUE;
+	}
+	if (p_address < p_program.length) {
+		return p_program[p_address];
+	} else {
+		if (p_reader.extraMemory.length == 0) {
+			return 0; // Day 9 : "Memory beyond the initial program starts with the value 0"
+		}
+		const indexF = findIndexExtraMemoryIntCode(p_reader.extraMemory, p_address);
+		if (indexF.found) {
+			return p_reader.extraMemory[indexF.index].value;
+		} else {				
+			return 0;
+		}
+	}
+}
+
+// Copied from utils_sorting !
+function findIndexExtraMemoryIntCode(p_extraMemory, p_address) {
+	var iMin = 0;
+	var iMax = p_extraMemory.length-1;
+	var iMid;
+	var elt;
+	while (true) {
+		if (iMax-iMin < 5) { // I want to play safe here !
+			var iNex = Math.min(iMin, iMax);
+			while (iNex < p_extraMemory.length && p_address >= p_extraMemory[iNex].address) {
+				if (p_address == p_extraMemory[iNex].address) {
+					return {found : true, index : iNex}
+				}
+				iNex++;
+			}
+			return {found : false, index : iNex};
+		}
+		iMid = Math.floor((iMin + iMax) / 2);
+		elt = p_extraMemory[iMid].address;
+		if (elt == p_address) {
+			return {found : true, index : iMid};
+		} else if (p_address < elt) {
+			iMax = iMid-1;
+		} else {			
+			iMin = iMid+1;
+		}
+	}
 }
