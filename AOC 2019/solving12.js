d31f1103384d7d8f1e9dfd1ad3c4f77cb9744d26
Benjamin <benjchauvet@gmail.com>
2024-02-20 21:49:30 +0100

    AOC 2019 : Solved day 12 (dancing moons)

--- AOC 2019/solving12.js
+++ AOC 2019/solving12.js
@@ -1,9 +1,26 @@
-const rawData = [
+const rawDataOfficial = [
 [-13, 14, -7],
 [-18, 9, 0],
 [0, -3, -3],
 [-15, 3, -13]
 ];
+const rawDataExample1 = [
+[-1,  0,  2],
+[ 2,-10, -7],
+[ 4, -8,  8],
+[ 3,  5, -1]
+];
+const rawDataExample2 = [
+[-8, -10, 0 ],
+[5, 5, 10   ],
+[2, -7,3    ],
+[9, -8,-3   ]
+]
+
+
+
+
+var rawData = rawDataExample2;
 
 var gPos;
 var gVelo;
@@ -69,7 +86,9 @@ function applyVelocities(p_i, p_j) {
 // ------------------------
 
 function conclusion_12_2() {
-	init();
+	rawData = rawDataOfficial;
+	return experiment_12_2();
+	/*init();
 	var tree = {branches : []}
 	// First step : find repeat for the first moon. Who knows, we may find a pattern...
 	var alreadyMet;
@@ -80,7 +99,7 @@ function conclusion_12_2() {
 			+ " : " + gPos[0][0] + "," + gPos[0][1] + "," + gPos[0][2]);
 		}
 		performStep();
-	}
+	}*/
 }
 
 // TODO : May be generalizable
@@ -127,3 +146,42 @@ function tryToInsert(p_subtree, p_eltToAdd) {
 function insertLeaf(p_subtree, p_leafElt) {
 	p_subtree.branches.push(p_leafElt);
 }
+
+function experiment_12_2() {
+	init();
+	var alreadyMet;
+	var count;
+	var countAll3 = 0;
+	var firstTimeCoorAt0 = [0, 0, 0];
+	for (var step = 0 ; step < 25000000 ; step++) {
+		count = 0;
+		for (var coor = 0 ; coor <= 2 ; coor++) {
+			for (var moon = 0 ; moon < rawData.length ; moon++) {
+				if (gVelo[moon][coor] != 0) {
+					break;
+				}
+			}
+			if (moon == rawData.length && step != 0) {
+				if (firstTimeCoorAt0[coor] == 0) {
+					firstTimeCoorAt0[coor] = step;
+					countAll3++;
+				}
+			}
+		}
+		if (countAll3 == 3) {
+			return lcm_all(firstTimeCoorAt0) * 2;
+		}
+		performStep();
+	}
+	return -1;
+}
+
+/*
+Raw data example 2 :
+ 
+(12) [0, 9487, 15523, 23610, 23745, 29424, 37305, 56265, 61471, 80518, 94815, 96001]
+(14) [0, 1404, 4939, 18239, 23322, 27964, 27998, 28367, 32733, 51073, 84873, 89429, 91338, 99996]
+(15) [0, 418, 4604, 5257, 6289, 17276, 20129, 33119, 33147, 42208, 47602, 52160, 55136, 57100, 78472]
+(7) [0, 37435, 44336, 58752, 81676, 82389, 90501]
+4686774924 = 1×2×2×3×13×13×983×2351 
+*/
